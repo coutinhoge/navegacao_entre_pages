a136abd87f36a96bcd64bdb50dee75d31977e8f4
gabriel-079 <contatogc871@gmail.com>
2023-03-06 11:19:02 -0300

    segunda versao

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import './pages/one_page.dart';
+import './pages/two_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Color.fromARGB(0, 0, 0, 0),
+          brightness: Brightness.dark),
+      home: OnePage(),
+    );
+  }
 }
